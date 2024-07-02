14bd4c7c7adc3810aa865a584840b0458e47678d
jtahun <jtahun@bk.ru>
2024-07-02 13:29:59 +0500

    cherryPick 2

--- first.js
+++ first.js
@@ -1,10 +1,4 @@
 console.log('one updated')
-console.log('one two')
-console.log('one three')
-console.log('one four')
-console.log('one five')
-console.log('updateUpdated')
-console.log('solve it')
-console.log('diff')
+
 //comment
 
