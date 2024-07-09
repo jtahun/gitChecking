524dda065d9efe71a1bbf2c241736a9891dd1577
jtahun <jtahun@bk.ru>
2024-07-09 17:04:56 +0500

    Revert "cherryPick 2"
    This reverts commit 14bd4c7c7adc3810aa865a584840b0458e47678d.

--- first.js
+++ first.js
@@ -1,4 +1,10 @@
 console.log('one updated')
-
+console.log('one two')
+console.log('one three')
+console.log('one four')
+console.log('one five')
+console.log('updateUpdated')
+console.log('solve it')
+console.log('diff')
 //comment
 
